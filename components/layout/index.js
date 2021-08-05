1657903a2b685a15eac23c4dc0f45376b2bc4327
dahaaw <dahaaw@gmail.com>
2021-08-05 17:07:00 +0700

    init

---
+++ components/layout/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import styles from '../../styles/layout.module.css';
+
+import React from 'react';
+
+const Layout = ({childrean}) => {
+    return (
+        <div className={styles.container}>
+            <Head>
+
+            </Head>
+
+            <header>
+
+            </header>
+
+            
+        </div>
+    );
+}
+
+export default Layout;
---
+++ pages/auth/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const login = () => {
+    return (
+        <div>
+            LOGIN
+        </div>
+    );
+}
+
+export default login;
